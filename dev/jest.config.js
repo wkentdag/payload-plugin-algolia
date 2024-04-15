bae424fb6273800a378dc737477a8d94b8c37ab7
will kent-daggett <3254957+wkentdag@users.noreply.github.com>
2024-04-14 20:46:38 -0700

    bugfix: ci tests

--- dev/jest.config.js
+++ dev/jest.config.js
@@ -4,12 +4,15 @@ module.exports = {
   globalTeardown: '<rootDir>/test/globalTeardown.ts',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.(t|j)sx?$': '<rootDir>/node_modules/@swc/jest',
+    '^.+\\.(t|j)sx?$': '<rootDir>/../node_modules/@swc/jest',
     // '^.+\\.(t)s?$': '<rootDir>/node_modules/@swc/jest',
   },
+  modulePaths: ['../src/'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/test/mocks/fileStub.js',
     '\\.(css|scss)$': '<rootDir>/test/mocks/fileStub.js',
+    '../src': '<rootDir>/../src',
+    'payoad-plugin-algolia': '<rootDir>/../src',
   },
 }
