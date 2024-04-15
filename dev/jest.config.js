8b70a4ec955fd7c01f842331d6eb3ef7d5ddf937
will kent-daggett <3254957+wkentdag@users.noreply.github.com>
2024-04-14 21:25:18 -0700

    bugfix: ci tests

--- dev/jest.config.js
+++ dev/jest.config.js
@@ -7,12 +7,9 @@ module.exports = {
     '^.+\\.(t|j)sx?$': '<rootDir>/../node_modules/@swc/jest',
     // '^.+\\.(t)s?$': '<rootDir>/node_modules/@swc/jest',
   },
-  modulePaths: ['../src/'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/test/mocks/fileStub.js',
     '\\.(css|scss)$': '<rootDir>/test/mocks/fileStub.js',
-    '../src': '<rootDir>/../src',
-    'payoad-plugin-algolia': '<rootDir>/../src',
   },
 }
